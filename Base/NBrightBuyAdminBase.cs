2ca80797ff02f0ada9941aa6a44b7694e27a2428
leedavi <dcl@nevoweb.com>
2014-07-10 17:15:02 +0200

    Base class

---
+++ Base/NBrightBuyAdminBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web;
+using System.Web.UI.WebControls;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+using NBrightDNN;
+using NBrightDNN.controls;
+using NEvoWeb.Modules.NB_Store;
+using NBrightCore.common;
+using System.Xml;
+using Nevoweb.DNN.NBrightBuy.Components;
+
+namespace Nevoweb.DNN.NBrightBuy.Base
+{
+    public class NBrightBuyAdminBase : NBrightBuyBase
+	{
+
+		protected override void OnInit(EventArgs e)
+		{
+            if (UserInfo.IsInRole("Editor") || UserInfo.IsInRole("Manager"))
+            {
+                base.OnInit(e);
+            }
+            else
+            {
+                // has no right to access, throw error page.
+                Response.Redirect("~/Error.aspx", true);
+            }
+        }
+
+
+	}
+}
