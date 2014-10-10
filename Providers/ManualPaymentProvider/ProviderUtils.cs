a9452e8d6c2f3133d5a46734a7febebf337b65b2
leedavi <dcl@nevoweb.com>
2014-10-10 10:30:35 +0200

    upd

---
+++ Providers/ManualPaymentProvider/ProviderUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using DotNetNuke.Entities.Portals;
+using NBrightCore.common;
+using NBrightDNN;
+using Nevoweb.DNN.NBrightBuy.Components;
+
+namespace ManualPaymentProvider
+{
+    public class ProviderUtils
+    {
+
+
+        public static String GetTemplateData(String templatename)
+        {
+            var controlMapPath = HttpContext.Current.Server.MapPath("/DesktopModules/NBright/NBrightBuy/Providers/ManualPaymentProvider");
+            var templCtrl = new NBrightCore.TemplateEngine.TemplateGetter(PortalSettings.Current.HomeDirectoryMapPath, controlMapPath, "Themes\\config", "");
+            var templ = templCtrl.GetTemplateData(templatename, Utils.GetCurrentCulture());
+            templ = Utils.ReplaceSettingTokens(templ, StoreSettings.Current.Settings());
+            templ = Utils.ReplaceUrlTokens(templ);
+            return templ;
+        }
+
+        public static NBrightInfo GetProviderSettings(String ctrlkey)
+        {
+            var info = (NBrightInfo)Utils.GetCache("ManualPaymentProvider" + PortalSettings.Current.PortalId.ToString(""));
+            if (info == null)
+            {
+                var modCtrl = new NBrightBuyController();
+
+                info = modCtrl.GetByGuidKey(PortalSettings.Current.PortalId, -1, "MANUALPAYMENT", ctrlkey);
+
+                if (info == null)
+                {
+                    info = new NBrightInfo(true);
+                    info.GUIDKey = ctrlkey;
+                    info.TypeCode = "MANUALPAYMENT";
+                    info.ModuleId = -1;
+                    info.PortalId = PortalSettings.Current.PortalId;
+                }
+
+                Utils.SetCache("ManualPaymentProvider" + PortalSettings.Current.PortalId.ToString(""), info);                
+            }
+
+            return info;
+        }
+
+
+    }
+}
