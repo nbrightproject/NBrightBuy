cf75390191fd265049249f2f9a973e293a2f6f7e
DNNMonster <DNNMonster@users.noreply.github.com>
2016-10-28 17:04:38 -0700

    duplicate article #82

--- Components/ProductData.cs
+++ Components/ProductData.cs
@@ -1319,7 +1319,7 @@ namespace Nevoweb.DNN.NBrightBuy.Components
                 dr3.ParentItemId = newid;
                 dr3.ItemID = -1;
                 dr3.GUIDKey = dr3.GUIDKey.Replace("x" + Info.ItemID.ToString(""), "x" + newid.ToString(""));
-                objCtrl.Update(dr);
+                objCtrl.Update(dr3);
             }            
 
 
