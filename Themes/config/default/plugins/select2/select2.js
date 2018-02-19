5c7d11ab6508b451ca2dceaef74efa2397c67d2f
leedavi <dcl@nevoweb.com>
2018-02-19 18:05:28 +0100

    upd itemlist

--- Themes/config/default/plugins/select2/select2.js
+++ Themes/config/default/plugins/select2/select2.js
@@ -2807,7 +2807,7 @@ the specific language governing permissions and limitations under the Apache Lic
                 enabledItem = $(
                     "<li class='select2-search-choice'>" +
                     "    <div></div>" +
-                    "    <a href='#' onclick='return false;' class='select2-search-choice-close' tabindex='-1'></a>" +
+                    "    <a href='javascript:void(0)' onclick='return false;' class='select2-search-choice-close' tabindex='-1'></a>" +
                     "</li>"),
                 disabledItem = $(
                     "<li class='select2-search-choice select2-locked'>" +
